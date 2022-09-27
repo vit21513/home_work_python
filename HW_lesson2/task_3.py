7b48e02ccbde43f505e3e27e884a96899b47c3e3
«vitaly» <vit21513@yandex.ru>
2022-09-27 17:31:41 +0300

    git task3

--- HW_lesson2/task_3.py
+++ HW_lesson2/task_3.py
@@ -1,1 +1,19 @@
-# Задать список из n чисел последовательности  (1 + 1/n)^n и вывести на экран их сумму.
+# Задать список из n чисел последовательности  (1 + 1/n)^n и 
+# вывести на экран их сумму.
+
+number = int(input("Введите  число n:  "))
+
+def list_numbers(num):
+    res=[]
+    for n in range(1,num+1):
+        form= (1+ 1/n)**n
+        res.append(form)
+    return res    
+
+    
+print(list_numbers(number)) 
+
+    
+
+    
+    
