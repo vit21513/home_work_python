4b178a09cf21d55b571985323ff05f0ea88680b1
«vitaly» <vit21513@yandex.ru>
2022-10-01 14:05:52 +0300

    add task 4 var21

--- HW_lesson3/task_4_recurs.py
+++ HW_lesson3/task_4_recurs.py
@@ -1,20 +1,21 @@
-
-
+# 4.Напишите программу, которая будет
+#  преобразовывать десятичное число в двоичное.
+# *Пример:*
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
 
 def num_to_bin(nem):
-    res=''
-    if  nem ==0:
-        return 
+    res = ''
+    if nem == 0:
+        return
     else:
-        res = res+str(nem%2)
-        num_to_bin(nem//2) 
-        print (res,end='')
-        
-    return     
+        res = res+str(nem % 2)
+        num_to_bin(nem//2)
+        print(res, end='')
+    return
 
-           
 
 number = int(input("Введите число:"))
 
 num_to_bin(number)
-
