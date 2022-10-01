01c87d2da83286fc15e2be4f84462b28372d2810
«vitaly» <vit21513@yandex.ru>
2022-10-01 14:03:30 +0300

    add task 4 var2

---
+++ HW_lesson3/task_4_recurs.py
@@ -0,0 +1,20 @@
+
+
+
+def num_to_bin(nem):
+    res=''
+    if  nem ==0:
+        return 
+    else:
+        res = res+str(nem%2)
+        num_to_bin(nem//2) 
+        print (res,end='')
+        
+    return     
+
+           
+
+number = int(input("Введите число:"))
+
+num_to_bin(number)
+
