16588e45a9b3b115aa59f3f8079d84b4efce4feb
«vitaly» <vit21513@yandex.ru>
2022-10-08 18:04:05 +0300

    yes all DonE!!!!

--- HW_lesson5/task_1.py
+++ HW_lesson5/task_1.py
@@ -3,16 +3,13 @@
 
 def sort_text(array):
 
-
     for i in array:
         if "абв" in i:
             array.remove(i)
-    list_array=" ".join(array)        
+    list_array = " ".join(array)
     return list_array
 
 
-
-
-sa= 'абвгдейка кинотеатр забвеность питон забвение прикол абвер'
-sort=sa.split()
-print(sort_text(sort))           
+sa = 'абвгдейка кинотеатр забвеность питон забвение прикол абвер'
+sort = sa.split()
+print(sort_text(sort))
