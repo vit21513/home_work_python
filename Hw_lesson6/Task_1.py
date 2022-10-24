d86878b3647da371ddb2f24b259fbec5a6f524df
vit21513 <vit21513@yandex.ru>
2022-10-24 08:53:32 +0300

    3

--- Hw_lesson6/Task_1.py
+++ Hw_lesson6/Task_1.py
@@ -1,23 +1,23 @@
 
-#Задача с семминара
+# Задача с семминара
 #  ' дом, окно, дверь, стена, кухня, стол, стул, дверь, дом, стул, стол, окно, стул'
 # Необходимо получить словарь, в котором ключи – слова, значения – количество слов в строке:
 # {'дом': 2, 'окно': 2, 'дверь': 2, 'стена': 1, 'кухня': 1, 'стол': 2, 'стул': 3}
 
 
-stroka= 'дом, окно, дверь, стена, кухня, стол, стул, дверь, дом, стул, стол, окно, стул'
-slovar= dict()
-spisok=stroka.split(", ")
-multi= set(spisok)
+stroka = 'дом, окно, дверь, стена, кухня, стол, стул, дверь, дом, стул, стол, окно, стул'
+slovar = dict()
+spisok = stroka.split(", ")
+multi = set(spisok)
 
 
-for i in  multi:
-    slovar[i]=spisok.count(i)
+for i in multi:
+    slovar[i] = spisok.count(i)
 
-print(slovar)    
+print(slovar)
 
 # вариант с  dict complication
 
-sorted_list = {k:spisok.count(k) for k in multi}  
+sorted_list = {k: spisok.count(k) for k in multi}
 
 print(sorted_list)
